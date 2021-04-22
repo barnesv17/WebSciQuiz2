14960169d79cddf58c4a6d50a03949a0288a9155
barnesv17 <virginiabarnes825@gmail.com>
2021-04-21 22:27:21 -0400

    background

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './the1975/dist/the1975')));
 
 app.get('/', (req, res) => {
   res.send("Hello world!");
